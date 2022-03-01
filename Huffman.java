df5c47169ebc03400b9a5c803d7d857887be5279
An-Fruit <92803192+An-Fruit@users.noreply.github.com>
2022-03-01 08:33:04 -0600

    Add files via upload

--- Huffman.java
+++ Huffman.java
@@ -4,17 +4,20 @@ import java.io.*;
 
 public class Huffman {
 	Map<Character, Integer> map;
-	Map<Character, String> codemap;
+	Map<Character, StringBuffer> codemap;
+	PrintWriter writer;
 	HuffmanNode root;
+	
+	
 	public Huffman() {
 		root = null;
 		map =  new HashMap<>();
 		codemap = new HashMap<>();
-		
 	}
 	
 	public void read(String filename) throws Exception{
 		Scanner file = new Scanner(new File(filename));
+	
 		//maps characters to their frequencies
 		while(file.hasNext()) {
 			String s = file.nextLine().trim();
@@ -30,8 +33,8 @@ public class Huffman {
 		
 	}
 	
-	
-	public void run() throws Exception{
+	//builds the huffman tree
+	public void run(String filename) throws Exception{
 		//creates a priority queue of HuffmanNodes
 		PriorityQueue<HuffmanNode> q = new PriorityQueue<>();
 		for(char c : map.keySet()) {
@@ -48,36 +51,53 @@ public class Huffman {
 		
 		root = q.poll();
 
-//		System.out.println(q);
-//		System.out.println(root.left);
-//		System.out.println(root.right);
-		
-		
+		for(char c : map.keySet()) {
+			encode(c, filename);
+			
+		}
 	}
-	public void encode(char c) {
+	
+	//given a character, traverse the tree and add on 1s and 0s
+	public void encode(char c, String filename) throws FileNotFoundException {
 		if(root == null) return;
 		else {
-			encode(root, c);
+			File file = new File(filename);
+			writer = new PrintWriter(file);
+			StringBuffer st = new StringBuffer("");
+			encode(root, c, st);
+			writer.close();
 		}
 	}
-	public void encode(HuffmanNode n, Character c) {
-		if(n == null || n.s == c) return;
-		if(map.get(c) > n.weight) {
-			if(codemap.get(c) == null) {
-				codemap.put(c, "");
-			}
-			codemap.put(c, codemap.get(c) + "1");
-			encode(n.right, c);
+	
+	public void encode(HuffmanNode n, Character c, StringBuffer code) {
+		if(n == null) return;
+		else if(n.s == c) {
+			System.out.println(c + " " + code);
+			writer.print(code);
 		}
-		else if (map.get(c) < n.weight) {
-			if(codemap.get(c) == null) {
-				codemap.put(c, "");
-			}
-			codemap.put(c, codemap.get(c) + "0");
-			encode(n.left, c);
+		else {
+			code.append('0');
+	        encode(n.left, c, code);
+	        code.deleteCharAt(code.length()-1);
+	        
+	        code.append('1');
+	        encode(n.right, c, code);
+	        code.deleteCharAt(code.length() - 1);
+	         
 		}
+		 
 		
 	}
+	
+	
+	//given a filename, go to that file and decode the characters
+	
+	public void decode(String outputfile) throws Exception{
+		Scanner output = new Scanner(new File(outputfile));
+		
+		
+	}
+	
 	
 	//printing the tree
 	public void preOrder() {
@@ -100,14 +120,6 @@ public class Huffman {
 	
 	
 	
-	
-	
-	public static void main(String[] args) throws Exception{
-		Huffman a = new Huffman();
-		a.read("taxi.txt");
-		a.run();
-		a.preOrder();
-	}
 	//huffman node with compareTo for the priorityqueue
 	 class HuffmanNode implements Comparable<HuffmanNode>{
 		 
